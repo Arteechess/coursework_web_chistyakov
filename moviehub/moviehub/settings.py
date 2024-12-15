7c190d59e8ef7316b54cd97299f75b30599d7617
Arteechess <artem@chistiakov.su>
2024-12-15 22:30:10 +0300

    extra changes

--- moviehub/moviehub/settings.py
+++ moviehub/moviehub/settings.py
@@ -116,6 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
+AUTH_USER_MODEL = 'movies.User'  # Укажите вашу кастомную модель пользователя
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
